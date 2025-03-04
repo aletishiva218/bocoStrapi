c330012b65d5b23c31a858cff4dc1c64f5ca9e8b
Shiva Aleti <aletishiva218@gmail.com>
2025-03-04 17:42:11 +0530

    updated database

--- config/database.js
+++ config/database.js
@@ -2,9 +2,16 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      connectionString: env('DATABASE_URL'),
-      ssl: { rejectUnauthorized: false }, // Ensure SSL is properly handled
+      host: env('DATABASE_HOST', 'db.ysscxwozzsnpbhdnipzm.supabase.co'),
+      port: env('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'postgres'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'dsjkdfsjK12ekj42349vjkfgqlkq`1`2'),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+        ca: env('DATABASE_SSL_CA', ''),
+      },
     },
-    pool: { min: 2, max: 10 },
+    debug: false,
   },
 });
